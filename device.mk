a94e8da07e50d9526d3873c79762685495aa1e08
Jyotiraditya <jyotiraditya@aospa.co>
2020-06-24 15:44:34 +0200

    onclite: Ship Display SVI configuration.
    Signed-off-by: Hadad <hadad@linuxmail.org>

--- device.mk
+++ device.mk
@@ -251,6 +251,9 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/SVIConfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/SVIConfig.xml
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
