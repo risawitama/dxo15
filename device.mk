59174e14e38ec988604ec2eac26fcc51235bbcf2
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-18 14:06:34 +0800

    onclite: Kang HotwordEnrollment from umi V14.0.5.0.TJBMIXM
    * SoundTrigger doesn't work with it for now for some reason, but at
      least this enables "Hey Google & Voice match" in Google Assistant
      app and makes "Hey Google" work when the screen is on
    
    Change-Id: Iaaba359e8480def2133cb3c24e5bfa716d755636

--- device.mk
+++ device.mk
@@ -90,6 +90,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/privapp-permissions-google-product-hotwordenrollment.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-product-hotwordenrollment.xml \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml
 
 # ANT
