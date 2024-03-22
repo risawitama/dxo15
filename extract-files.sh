6ed23bdb890fa33af53d2bcd52aacaf08b1d36af
hadad <hadad@linuxmail.org>
2024-03-22 20:50:21 +0700

    onclite: patch ELF lib_lowlight.so to load libstdc++_vendor.so.
    Change-Id: Iace5005a65ccfc17640c06063107ee9f39565a46

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
     vendor/bin/pm-service)
     grep -q "libutils-v33.so" "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
         ;;
+    vendor/lib/lib_lowlight.so)
+        "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+        ;;
     esac
 }
 
