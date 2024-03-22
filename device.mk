cf5c37ccd29e4348f6aea4c7e08c9b935b2b949d
Aaron Kling <webgeek1234@gmail.com>
2024-03-22 08:05:53 -0500

    onclite: Uprev usb hal to 1.3
    Change-Id: I5d34c71663b31fe03b0d28f8c76e5a4f8cd2385c

--- device.mk
+++ device.mk
@@ -427,7 +427,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Vibrator
 TARGET_USES_DEVICE_SPECIFIC_VIBRATOR := true
