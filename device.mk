1ec4f1ca659355efebd58b69905277f088d2a91a
hadad <hadad@linuxmail.org>
2024-03-22 13:25:11 +0700

    onclite: build more dependency of camera packages.

--- device.mk
+++ device.mk
@@ -131,11 +131,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
     libdng_sdk.vendor \
+    libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0 \
