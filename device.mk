f89be060fa8857442c031687fedfed68657864d9
hadad <hadad@linuxmail.org>
2024-03-29 20:00:39 +0700

    onclite: Switch to IBiometricsFingerprint 2.3 HAL.

--- device.mk
+++ device.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.onclite
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
