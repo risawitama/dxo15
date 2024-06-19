d2dff0f925700965fadc6874cefa3f22bbc831ca
Giovanni Ricca <giovanniricca@proton.me>
2024-06-20 01:18:02 +0200

    onclite: Move `atcmdfwd` from framework manifest to framework compatibility matrix
    Change-Id: I9943e6510a014523c3e13e2ff3599c5a1bd6135f
    
    * onclite: also unset kernel target level in manifest.
    
    Co-authored-by: Hadad <hadad@linuxmail.org>
    Signed-off-by: Hadad <hadad@linuxmail.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,12 +135,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_onclite
