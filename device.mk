45101ff4b4d93dab4ceb80a0b68457ac8a540696
Bharath <bharath@teamb58.org>
2024-09-06 19:23:40 +0700

    onclite: Switch to AIDL for widevine
    Recent changes in GTS now expectes Android 13 to use AIDL version
    instead of the old HIDL version.
    
    Test: run gts -m WvtsDeviceTestCases
    Signed-off-by: Hadad <hadad@linuxmail.org>
    Change-Id: Ia290ea9192070476d99b59d15ddd82dbf9a2fab2

--- device.mk
+++ device.mk
@@ -247,7 +247,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
+    android.hardware.drm-V1-ndk.vendor \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
